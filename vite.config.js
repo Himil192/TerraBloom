e7784fddd3d9799a0a571f9d3c94a0f7bc0425f5
Himil192 <himilprajapati2611@gmail.com>
2025-04-20 02:10:57 +0530

    minor mistake in vite config base: /TerraBloom/ i wrote /terrabloom

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/terrabloom',
+  base: '/TerraBloom/',
   plugins: [
     react(),
     tailwindcss(),
