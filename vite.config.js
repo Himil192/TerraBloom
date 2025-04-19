754fa47d15e1cde8760ee0bf408d938dc9c341a2
Himil192 <himilprajapati2611@gmail.com>
2025-04-20 01:59:43 +0530

    add gh-pages install

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/terrabloom/',
+  base: '/terrabloom',
   plugins: [
     react(),
     tailwindcss(),
